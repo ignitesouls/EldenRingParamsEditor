d936ceda5433560574775d588e3fff799a6508e6
psiphicode <psiphisolutions@gmail.com>
2025-06-13 11:57:16 -0700

    fix: add per-file license specification

--- EldenRingParamsEditor/ParamsEditor.Metadata.cs
+++ EldenRingParamsEditor/ParamsEditor.Metadata.cs
@@ -1,4 +1,5 @@
-﻿using DotNext.Collections.Generic;
+﻿// SPDX-License-Identifier: GPL-3.0-only
+using DotNext.Collections.Generic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
